aa265c23596b1132c87332496d5c50b7240a446e
Yuki Nagai <ynagai@r.recruit.co.jp>
2018-08-11 07:06:50 +0900

    Initial commit

---
+++ LoadMAP/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LoadMAP
+//
+//  Created by Yuki Nagai on 2018/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
